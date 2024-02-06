d4db39fa9f6b15eab8922374aab94e20db708f69
Amar5246 <amar.d.phadatare@gmail.com>
2024-02-06 18:49:18 +0530

    Add employee details Endpoint added

---
+++ Backend Spring/hrm/src/main/java/com/hrms/app/controller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.hrms.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.hrms.app.request.EmployeeRequest;
+import com.hrms.app.service.IEmployeeService;
+
+import jakarta.validation.Valid;
+
+@RestController 
+@RequestMapping("/Employee")
+@CrossOrigin(origins = "")
+public class EmployeeController {
+	
+	//service layer dependency injection(DI) 
+	@Autowired
+	private IEmployeeService empService;  
+	
+	//endpoint for adding new employee details
+	@PostMapping
+	public ResponseEntity<?> addEmpDetails(@RequestBody @Valid EmployeeRequest empReq )
+	{
+		try {
+			//calling EmpService method for adding employee 
+			return new ResponseEntity<>(empService.addEmployee(empReq),HttpStatus.OK);
+		}
+		catch(RuntimeException e)
+		{
+			System.out.println("err in controller "+e);
+			//return err mesg wrapped in DTO : ApiResp
+			return  ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body(e.getMessage());
+		}
+	}
+	
+}
--- Backend Spring/hrm/src/main/java/com/hrms/app/service/EmployeeServiceImpl.java
+++ Backend Spring/hrm/src/main/java/com/hrms/app/service/EmployeeServiceImpl.java
@@ -1,8 +1,10 @@
 package com.hrms.app.service;
 
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.hrms.app.model.Employee;
 import com.hrms.app.repo.IEmployeeRepository;
 import com.hrms.app.request.EmployeeRequest;
 import com.hrms.app.response.EmployeeDto;
@@ -11,18 +13,27 @@ import com.hrms.app.response.EmployeeDto;
 public class EmployeeServiceImpl implements IEmployeeService {
 	@Autowired 
 	private IEmployeeRepository empRepo;
+	
+	@Autowired
+	private ModelMapper mapper;
 
 	@Override
 	public EmployeeDto addEmployee(EmployeeRequest empReq) {
 		//validate password and confirm password  
 		if(empReq.getConfirmPassword().equals(empReq.getPassword()))
 		{
+			//convert EmployeeRequest object to Employee
+			Employee emp=mapper.map(empReq,Employee.class);  
+			//save emp object in database 
+			empRepo.save(emp); 
+			System.out.println("Employee addes with id: "+emp.getEmpId()); 
 			
+			return mapper.map(emp,EmployeeDto.class);
 		} 
 		else {
-			
+			throw new RuntimeException("password doesnot match please re-enter password");
 		}
-		return null;
+		
 	} 
 	
 	
