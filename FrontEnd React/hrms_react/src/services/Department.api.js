e1622732dba09953cd06de62edcfe64ab2eb7925
akshaydudhe943 <akshaydudhe943@gmail.com>
2024-02-08 12:32:16 +0530

    preetier error resolve

--- FrontEnd React/hrms_react/src/services/Department.api.js
+++ FrontEnd React/hrms_react/src/services/Department.api.js
@@ -2,23 +2,15 @@ import axios from 'axios'
 import { DepartmentUrl } from '../urls/Department.url'
 
 class DepartmentApiService {
-
-    async getDepartmentList() {
-        //made async because http methods are asynchronous  in nature
-        try {
-            const url = DepartmentUrl.baseDepartmentUrl
-            const response = await axios.get(url)
-
-            return response.data // return the response data
-        } catch (error) {
-            throw new Error('department fetching failed. Please try again.')
-        }
+  async getDepartmentList() {
+    //made async because http methods are asynchronous  in nature
+    try {
+      const url = DepartmentUrl.baseDepartmentUrl
+      const response = await axios.get(url)
+      return response.data // return the response data
+    } catch (error) {
+      throw new Error('department fetching failed. Please try again.')
     }
-
+  }
 }
-
-export default new DepartmentApiService();
-
-
-
-
+export default new DepartmentApiService()
--- FrontEnd React/hrms_react/src/services/Employee.api.js
+++ FrontEnd React/hrms_react/src/services/Employee.api.js
@@ -2,19 +2,15 @@ import axios from 'axios'
 import { EmployeeUrl } from '../urls/Employee.url'
 
 export class EmployeeApiService {
-
-
-    async addEmployee(employee) {
-        //made async because http methods are asynchronous  in nature
-        try {
-            const url = EmployeeUrl.baseEmployeeUrl
-            const response = await axios.post(url, employee)
-
-            return response.data // return the response data
-        } catch (error) {
-            throw new Error('Employee regestraton failed. Please try again.')
-        }
+  async addEmployee(employee) {
+    //made async because http methods are asynchronous  in nature
+    try {
+      const url = EmployeeUrl.baseEmployeeUrl
+      const response = await axios.post(url, employee)
+      return response.data // return the response data
+    } catch (error) {
+      throw new Error('Employee regestraton failed. Please try again.')
     }
+  }
 }
-
-export default new EmployeeApiService();
+export default new EmployeeApiService()
