8ddf37a28c1cd6b958f539749938940f091bd2eb
ERaniket <aniketp1404@gmail.com>
2024-03-02 00:20:33 +0530

    fetch attendance Backend API fixed

--- Backend Spring/hrm/src/main/java/com/hrms/app/model/Attendance.java
+++ Backend Spring/hrm/src/main/java/com/hrms/app/model/Attendance.java
@@ -3,13 +3,13 @@ package com.hrms.app.model;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Data
-@Document(collation = "attendance")
+@Document(collection = "attendance")
 public class Attendance {
 
 	@Id
